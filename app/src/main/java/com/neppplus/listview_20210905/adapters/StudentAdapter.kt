86c86ddc83d778b13e95981c97d464296b4b3d5d
조경진 <cho881020@gmail.com>
2021-09-05 14:14:24 +0900

    주소도 실제 데이터 반영

--- app/src/main/java/com/neppplus/listview_20210905/adapters/StudentAdapter.kt
+++ app/src/main/java/com/neppplus/listview_20210905/adapters/StudentAdapter.kt
@@ -40,9 +40,12 @@ class StudentAdapter(
 
 //        row의 내부에서, 필요한 텍스트뷰 등 UI요소들을 가져오자.
         val nameTxt = row.findViewById<TextView>( R.id.nameTxt )
+        val addressTxt = row.findViewById<TextView>( R.id.addressTxt )
+        val ageTxt = row.findViewById<TextView>(R.id.ageTxt)
 
 
         nameTxt.text = data.name
+        addressTxt.text = data.address
 
 //        만들어진 row를 getView 작업의 결과물로 지정.
 
