bbeee800af102c1e0d11e680ee1371dbc11a7762
조경진 <cho881020@gmail.com>
2021-09-05 12:50:06 +0900

    getView 함수 오버라이딩  - convertView 검사 (tempRow 변수로 대체)  - 내용물이 없다면, 새로 하나 채워주자.

--- app/src/main/java/com/neppplus/listview_20210905/adapters/StudentAdapter.kt
+++ app/src/main/java/com/neppplus/listview_20210905/adapters/StudentAdapter.kt
@@ -2,7 +2,10 @@ package com.neppplus.listview_20210905.adapters
 
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.neppplus.listview_20210905.R
 import com.neppplus.listview_20210905.datas.StudentData
 
 class StudentAdapter(
@@ -17,5 +20,13 @@ class StudentAdapter(
 
     val mInflater = LayoutInflater.from(mContext)
 
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+        if (tempRow == null) {
+            tempRow =  mInflater.inflate(R.layout.student_list_item,  null)
+        }
+
+    }
 
 }
